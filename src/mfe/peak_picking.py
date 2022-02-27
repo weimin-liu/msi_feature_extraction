fa65df35c7b8fe062d3f65bf30a5fbae741558f9
Weimin Liu <wliu@marum.de>
2022-02-27 13:11:27 +0100

    fix bugs

--- src/mfe/peak_picking.py
+++ src/mfe/peak_picking.py
@@ -1,23 +1,16 @@
+import itertools
+
 import numpy as np
 import pandas as pd
 import tqdm
 from scipy import interpolate
-from skimage.feature import graycomatrix
+from skimage.feature import graycomatrix, graycoprops
 from scipy.interpolate import interpolate
 from skimage.exposure import exposure
 import cv2
 
 
 class GLCMPeakRanking:
-    # === The matrix used to calculate texture score === #
-    C = np.array([
-        [4, 2, 1, 0, 0],
-        [2, 1, 0, 0, 0],
-        [1, 0, 0, 0, 1],
-        [0, 0, 0, 1, 2],
-        [0, 0, 1, 2, 4]
-    ])
-
     def __init__(self,
                  fill_value=0,
                  interpolation='blur',
@@ -37,36 +30,27 @@ class GLCMPeakRanking:
             bins with a number of q, and the intensities are then replaced with the label (integer number). Currently,
             q is fixed to 5 for the convenience of the following structure score calculation.
         """
-        if self.C.shape[0] == self.C.shape[0] == q:
-            self.interpolation = interpolation
-            self.fill_value = fill_value
-            self.remove_whitespace = remove_whitespace
-            self.stretch_contrast = stretch_contrast
-            self.contrast_lim = contrast_lim
-            self.blur_filter = blur_filter
-            self.q = q
-            self.feature_table = pd.DataFrame()
-            self.images = list()
-            self.score = pd.DataFrame()
-            self.mzs = np.array([])
-        else:
-            raise ValueError('The number of zones are not equal to the calculation matrix!')
+        self.interpolation = interpolation
+        self.fill_value = fill_value
+        self.remove_whitespace = remove_whitespace
+        self.stretch_contrast = stretch_contrast
+        self.contrast_lim = contrast_lim
+        self.blur_filter = blur_filter
+        self.q = q
+        self.feature_table = pd.DataFrame()
+        self.images = list()
+        self.results = list()
+        self.prop = ['dissimilarity', 'homogeneity', 'energy',  'correlation']
+        self.mzs = np.array([])
 
-    def _reset(self):
-        if len(self.score) != 0:
-            del self.feature_table
-            del self.images
-            del self.score
-
-    def fit(self, feature_table: pd.DataFrame, angle=0):
+    def fit(self, feature_table: pd.DataFrame, dist, angle):
         """
         Parameters:
         --------
         """
-        self._reset()
-        return self.partial_fit(feature_table, angle=angle)
+        return self.partial_fit(feature_table, dist, angle)
 
-    def partial_fit(self, feature_table: pd.DataFrame, angle=0):
+    def partial_fit(self, feature_table: pd.DataFrame, dist, angle):
         """
         This is an example of how to get structured ion image from feature table.
 
@@ -91,9 +75,8 @@ class GLCMPeakRanking:
         arr = dirty_df.drop(columns=['x', 'y']).to_numpy()
 
         mzs = list(dirty_df.drop(columns=['x', 'y']).columns)
-        self.mzs = np.array([float(mz) for mz in mzs])
 
-        t = dict()
+        self.mzs = np.array([float(mz) for mz in mzs])
 
         print('Processing each ion image')
 
@@ -108,43 +91,15 @@ class GLCMPeakRanking:
                                blur_filter=self.blur_filter,
                                fill_value=self.fill_value)
 
-            score = self.cal_structure_score(image, angle)
+            self.cal_glcm_prop(image, dist, angle)
 
             self.images.append(image)
-
-            t[mz] = score
-
-        self.score = pd.DataFrame.from_dict(t, orient='index')
 
         self.images = np.array(self.images)
 
         self.images[np.isnan(self.images)] = 0
 
-    @property
-    def mzs_sorted(self):
-        """
-        Return mz values of the ion images sorted by ranking structured score
-
-        Returns:
-        """
-        score_sorted = self.score.sort_values(by=0, ascending=False)
-        mzs_sorted = np.array(list(score_sorted.index))
-        return mzs_sorted
-
-    @property
-    def images_sorted(self):
-        """
-        return image arrays that are sorted by ranking structured score
-
-        Returns:
-
-        """
-        score = self.score.to_numpy().flatten()
-        score_increase = score.argsort()
-        images_sorted = self.images[score_increase[::-1]]
-        return images_sorted
-
-    def cal_structure_score(self, image, angle):
+    def cal_glcm_prop(self, image, dist, angle):
         """
         Quantize the image and then calculate its structure score using grey-level co-occurrence matrix. See
         https://scikit-image.org/docs/0.7.0/api/skimage.feature.texture.html
@@ -159,17 +114,18 @@ class GLCMPeakRanking:
             The structure score of the image. The higher the score, the more structured the ion image.
 
         """
-        nan_flg = False
 
         df = pd.DataFrame(image.reshape(-1, 1))
 
         n_spots = np.count_nonzero(df[0].to_numpy())
 
-        bin_labels = list(range(self.q))
+        bin_labels = list(range(1, self.q + 1))
+
+        result = list()
 
         if n_spots == 0:
 
-            score = 0
+            result.append(np.nan)
 
         else:
 
@@ -178,74 +134,66 @@ class GLCMPeakRanking:
                 if df[0].min() == 0:
                     df = df.replace(0, np.nan)
 
-                    nan_flg = True
-
                 df['quantile_ex_1'] = pd.qcut(df[0], q=self.q, labels=bin_labels, duplicates='drop')
 
                 im_quantized = df['quantile_ex_1'].to_numpy().reshape(image.shape)
 
-                im_quantized[np.isnan(im_quantized)] = self.q
+                im_quantized[np.isnan(im_quantized)] = 0
 
                 im_quantized = im_quantized.astype(int)
 
-                if nan_flg:
+                gcm = graycomatrix(im_quantized, dist, angle, levels=self.q + 1)
 
-                    gcm = graycomatrix(im_quantized, [1], [angle], levels=self.q + 1)[:, :, 0, 0]
+                for d, theta in itertools.product(range(len(dist)), range(len(angle))):
 
-                    gcm = gcm[0:-1, 0:-1]
-
-                else:
-
-                    gcm = graycomatrix(im_quantized, [1], [angle], levels=self.q)[:, :, 0, 0]
-
-                score = np.sum(np.multiply(gcm, self.C)) / n_spots
+                    for key in self.prop:
+                        result.append(graycoprops(gcm, key)[d, theta])
 
             except ValueError:
+                result.append(np.nan)
 
-                score = 0
+        self.results.append(result)
 
-        return score
-
-    def transform(self, threshold):
-        """
-        Select the structured peaks (thus more meaningful) in the feature table and return the result
-
-        Parameters:
-        --------
-            t_df: a DataFrame object with ranked mass-to-charge ratios
-
-            feature_table: a Dataframe object
-
-            threshold: above which the peaks will be preserved
-
-        Returns:
-        --------
-            feature_table_picked: a Dataframe object with only peaks that have above threshold structure score
-
-            deflated_arr_picked: an array with picked ion images
-        """
-
-        t_df_arr = self.score.to_numpy().flatten()
-
-        mask = t_df_arr >= threshold
-
-        images_picked = self.images[mask]
-
-        images_picked = np.array(images_picked)
-
-        t_df = self.score[self.score >= threshold]
-
-        t_df = t_df.dropna()
-
-        sel_mzs = list(t_df.index)
-
-        sel_mzs.extend(['x', 'y'])
-
-        sel_columns = [col for col in list(self.feature_table.columns) if col in sel_mzs]
-
-        feature_table_picked = self.feature_table[sel_columns]
-
-        return feature_table_picked, images_picked
+    # def transform(self, threshold):
+    #     """
+    #     Select the structured peaks (thus more meaningful) in the feature table and return the result
+    #
+    #     Parameters:
+    #     --------
+    #         t_df: a DataFrame object with ranked mass-to-charge ratios
+    #
+    #         feature_table: a Dataframe object
+    #
+    #         threshold: above which the peaks will be preserved
+    #
+    #     Returns:
+    #     --------
+    #         feature_table_picked: a Dataframe object with only peaks that have above threshold structure score
+    #
+    #         deflated_arr_picked: an array with picked ion images
+    #     """
+    #
+    #     t_df_arr = self.score.to_numpy().flatten()
+    #
+    #     mask = t_df_arr >= threshold
+    #
+    #     images_picked = self.images[mask]
+    #
+    #     images_picked = np.array(images_picked)
+    #
+    #     t_df = self.score[self.score >= threshold]
+    #
+    #     t_df = t_df.dropna()
+    #
+    #     sel_mzs = list(t_df.index)
+    #
+    #     sel_mzs.extend(['x', 'y'])
+    #
+    #     sel_columns = [col for col in list(self.feature_table.columns) if col in sel_mzs]
+    #
+    #     feature_table_picked = self.feature_table[sel_columns]
+    #
+    #     return feature_table_picked, images_picked
 
 
 def whitespace_removal(image: np.ndarray):
